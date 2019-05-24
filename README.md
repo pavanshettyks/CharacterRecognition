95af3faab36f7714ca9ff4dd098b817a5f4f7aba
Panna <pavanksshetty@gmail.com>
2019-05-24 12:20:15 -0700

    test 1

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Image character recognition with google vision API
 
 Install google-vision API:
 pip install google-cloud-vision
+
+https://cloud.google.com/vision/docs/libraries#client-libraries-install-python
--- char_recog.py
+++ char_recog.py
@@ -1,7 +1,11 @@
+from google.cloud import vision
+import os, io, types
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/Pavan/Project/GCP/client123-1c6497d3482c.json"
+
 client = vision.ImageAnnotatorClient()
-with io.open(image_file, 'rb') as image_file:
+path = "Payment.png"
+with io.open(path, 'rb') as image_file:
         content = image_file.read()
-image = types.Image(content=content)
+image = vision.types.Image(content=content)
 response = client.document_text_detection(image=image)
 document = response.full_text_annotati
-djdd
