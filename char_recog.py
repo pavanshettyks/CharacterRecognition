e48a3b7cca9eeff505b3ea7da960c627cf0bc189
Panna <pavanksshetty@gmail.com>
2019-05-24 11:29:17 -0700

    initial

---
+++ char_recog.py
@@ -0,0 +1,6 @@
+client = vision.ImageAnnotatorClient()
+with io.open(image_file, 'rb') as image_file:
+        content = image_file.read()
+image = types.Image(content=content)
+response = client.document_text_detection(image=image)
+document = response.full_text_annotati
